080d248049746d53dc93990afe1844a3f150c61f
marcos-acosta <macosta1625@gmail.com>
2025-09-26 02:02:32 -0400

    Safari doesn't play nice with buttons and flex boxes

--- app/src/components/MatchPreview.tsx
+++ app/src/components/MatchPreview.tsx
@@ -25,33 +25,34 @@ export default function MatchPreview(props: MatchPreviewProps) {
     (props.match.matchData.competingTrips[0].routeId === winner ? 2 : 1);
 
   return (
-    <button
-      className={styles.matchPreviewInnerContainer}
-      onClick={props.select}
-    >
-      <div className={styles.matchPreviewHeader}>
-        <div className={styles.roundNameContainer}>{roundName}</div>
-        <div className={styles.matchPreviewStatusContainer}>{matchStatus}</div>
-      </div>
-      <div className={styles.matchPreviewWhiteLine} />
-      <div className={styles.matchPreviewLinesContainer}>
-        {props.match.matchData.competingTrips.map((trip, i) => (
-          <TripPreview
-            key={trip.routeId}
-            tripData={trip}
-            numStops={props.match.matchData.numStopsToFinish}
-            row={i + 1}
-            finished={winner !== undefined}
-            won={winner === trip.routeId}
-            victoryType={victoryType}
-          />
-        ))}
-        {winner && (
-          <div
-            className={styles.previewLoserShader}
-            style={{ gridRow: `${loserRow}` }}
-          />
-        )}
+    <button onClick={props.select}>
+      <div className={styles.matchPreviewInnerContainer}>
+        <div className={styles.matchPreviewHeader}>
+          <div className={styles.roundNameContainer}>{roundName}</div>
+          <div className={styles.matchPreviewStatusContainer}>
+            {matchStatus}
+          </div>
+        </div>
+        <div className={styles.matchPreviewWhiteLine} />
+        <div className={styles.matchPreviewLinesContainer}>
+          {props.match.matchData.competingTrips.map((trip, i) => (
+            <TripPreview
+              key={trip.routeId}
+              tripData={trip}
+              numStops={props.match.matchData.numStopsToFinish}
+              row={i + 1}
+              finished={winner !== undefined}
+              won={winner === trip.routeId}
+              victoryType={victoryType}
+            />
+          ))}
+          {winner && (
+            <div
+              className={styles.previewLoserShader}
+              style={{ gridRow: `${loserRow}` }}
+            />
+          )}
+        </div>
       </div>
     </button>
   );
