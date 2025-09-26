27b50482de99bbe39ab40d96be0047f1627b9575
marcos-acosta <macosta1625@gmail.com>
2025-09-25 23:31:09 -0400

    Automatically hide many match previews unless expanded

--- app/src/components/MatchesDashboard.tsx
+++ app/src/components/MatchesDashboard.tsx
@@ -1,17 +1,52 @@
+import { useState } from "react";
 import { Match } from "../interfaces";
 import styles from "./../../page.module.css";
 import MatchPreview from "./MatchPreview";
+import Image from "next/image";
 
 interface MatchesDashboardProps {
   matches: Match[];
 }
 
+const SHORTENED_LIST_LENGTH = 2;
+
 export default function MatchesDashboard(props: MatchesDashboardProps) {
+  const [showAllPreviews, setShowAllPreviews] = useState(false);
+
+  const matchesToShow = showAllPreviews
+    ? props.matches
+    : props.matches.slice(0, SHORTENED_LIST_LENGTH);
+  const matchesToHide = props.matches.length > SHORTENED_LIST_LENGTH;
+
   return (
     <div className={styles.matchesDashboardInnerContainer}>
-      {props.matches.map((match) => (
-        <MatchPreview match={match} key={match.matchId} />
-      ))}
+      <div className={styles.matchesDashboardGrid}>
+        {matchesToShow.map((match) => (
+          <MatchPreview match={match} key={match.matchId} />
+        ))}
+      </div>
+      {matchesToHide && (
+        <div className={styles.showHideContainer}>
+          <div className={styles.showHideTextAndArrowContainer}>
+            <button
+              className={styles.showHideText}
+              onClick={() => setShowAllPreviews(!showAllPreviews)}
+            >
+              {showAllPreviews ? "Collapse" : "Show all"}
+            </button>
+            <div className={styles.arrowContainer}>
+              <Image
+                className={styles.expandArrow}
+                src="/arrows/right-arrow.svg"
+                width="15"
+                height="15"
+                alt="Arrow"
+                style={{ rotate: `${showAllPreviews ? 270 : 90}deg` }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
